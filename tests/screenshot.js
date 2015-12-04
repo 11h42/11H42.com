b835d711b475a8a84e6ea078bbc5cf79079159c8
Édouard Lopez <elopez@akema.fr>
2015-12-04 12:26:53 +0100

    improve error management and filename format

--- tests/screenshot.js
+++ tests/screenshot.js
@@ -1,9 +1,15 @@
 const Pageres = require('pageres');
 
-const pageres = new Pageres({delay: 2})
+const pageres = new Pageres({
+        delay: 2,
+        filename: '<%= date %>-<%= time %>.<%= url %>-<%= size %><%= crop %>'
+    })
     .src('11h42.com', ['480x320', '1024x768', 'iphone 5s'], {crop: true})
     .dest(__dirname + '/screenshots')
     .run()
     .then(function () {
-           console.log('done')
+       console.log('done')
+    })
+    .catch(function () {
+       console.error('fail');
     });
