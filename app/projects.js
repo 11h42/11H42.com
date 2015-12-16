6c4f9bdddd640d8f09300116a2cfdbe3abeaad63
Édouard Lopez <dev+git@edouard-lopez.com>
2015-12-16 17:52:33 +0100

    start to test projects.js

---
+++ app/projects.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    class Projects {
+        constructor() {
+        }
+
+        breath() {
+            return true;
+        }
+    }
+    module.exports = Projects;
+}());
---
+++ tests/app.tests.js
@@ -0,0 +1,13 @@
+import * as assert from 'assert';
+import * as Projects from '../app/projects';
+
+(function () {
+    'use strict';
+
+    describe('projects', function () {
+        it('should be alive', function () {
+            var isAlive = Projects.breath();
+            assert.isTrue(isAlive);
+        });
+    });
+}());
