4a31fcef349c7528345fe0653b2ddef5a374f060
Édouard Lopez <dev+git@edouard-lopez.com>
2015-12-18 00:00:28 +0100

    import semantic-ui CSS in index.html and JS in app.js, use default structure

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
 import * as stylesheet from '../assets/styles/app.scss';
 import jQuery from '../node_modules/jquery/dist/jquery';
 import $ from '../node_modules/jquery/dist/jquery';
+import * as semanticUi from '../node_modules/semantic-ui/dist/semantic';
