37e99701a10d15979e81fa9e1621b86d4ed10be9
Édouard Lopez <dev+git@edouard-lopez.com>
2015-12-17 16:57:24 +0100

    remove useless import in app

--- app/index.js
+++ app/index.js
@@ -1,14 +1,1 @@
-import * as nodeStatic from 'node-static';
 import * as stylesheet from '../assets/styles/app.scss';
-import * as http from 'http';
-
-var file = new nodeStatic.Server('./dist');
-
-http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        file.serve(request, response);
-    }).resume();
-}).listen(8080);
