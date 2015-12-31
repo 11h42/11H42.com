5e08d712810b3da664901cbfddbfd1082a02a3f3
Édouard Lopez <dev+git@edouard-lopez.com>
2015-12-31 20:07:09 +0100

    comment angular2 from index.js for now (need to fix ngFor)

--- app/index.js
+++ app/index.js
@@ -5,12 +5,12 @@ window.$ = $;
 window.jQuery = jQuery;
 
 
-import 'zone.js/lib/browser/zone-microtask';
-import 'reflect-metadata';
-import 'babel-polyfill';
-
-import {provide} from 'angular2/core';
-import {bootstrap} from 'angular2/platform/browser';
-
-import {App} from './app.component'
-bootstrap(App, []);
+//import 'zone.js/lib/browser/zone-microtask';
+//import 'reflect-metadata';
+//import 'babel-polyfill';
+//
+//import {provide} from 'angular2/core';
+//import {bootstrap} from 'angular2/platform/browser';
+//
+//import {App} from './app.component'
+//bootstrap(App, []);
