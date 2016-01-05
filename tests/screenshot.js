239105bdde4339c4f11d6df3c486c2cb79c5ae76
Édouard Lopez <dev+git@edouard-lopez.com>
2016-01-05 16:59:07 +0100

    typo

--- tests/screenshot.js
+++ tests/screenshot.js
@@ -1,15 +1,15 @@
 const Pageres = require('pageres');
 
 const pageres = new Pageres({
-        delay: 2,
-        filename: '<%= date %>-<%= time %>.<%= url %>-<%= size %><%= crop %>'
-    })
+    delay: 2,
+    filename: '<%= date %>-<%= time %>.<%= url %>-<%= size %><%= crop %>'
+})
     .src('11h42.com', ['w3counter'], {crop: true})
     .dest(__dirname + '/screenshots')
     .run()
     .then(function () {
-       console.log('done')
+        console.log('done')
     })
     .catch(function () {
-       console.error('fail');
+        console.error('fail');
     });
